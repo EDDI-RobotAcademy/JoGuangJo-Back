8dbad984ba3802ce7ce06980cd53f466c254e1de
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-03-19 23:28:28 +0900

    init:[박지영] byl_process 백엔드 프로젝트 등록
     [x] build.gradle 설정 완료
     [x] application.yaml 설정 완료
     [x] settings - file encoding - utf-8 설정 완료
     [x] help - VM options 설정 완료
     [x] settings - buld tools - gradle - build and run : intelliJ 설정 완료
     [x] SDK - Java 11 설정 완료
     [x] jpa, mysql (byl_db), redis (redis_boot) 연동 완료
    
    이제 팀원들은 모두 각자 fork한 repository를 sync -> pull -> clone 하세요.
    
    자기 노트북에 clone 을 했고, mySQL에 `byl_db` 스키마와 `eddi` 계정을 생성해 서로 연동했고, docker에 `reddis_boot` 를 생성했으면,
    intelliJ로 백엔드 repo 를 열고 BylProcessApplication 을 실행해 보세요.

---
+++ byl_process/src/main/java/com/jgj/byl_process/BylProcessApplication.java
@@ -0,0 +1,13 @@
+package com.jgj.byl_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BylProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BylProcessApplication.class, args);
+	}
+
+}
---
+++ byl_process/src/test/java/com/jgj/byl_process/BylProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgj.byl_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BylProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

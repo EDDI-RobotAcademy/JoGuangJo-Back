d23c6c7fe14706e92fe1850607c3d97c0e90bb48
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2023-04-06 05:20:47 +0900

    fix: [안성희] "CI 구성중"

--- src/test/java/com/jgj/byl_process/BylProcessApplicationTests.java
+++ src/test/java/com/jgj/byl_process/BylProcessApplicationTests.java
@@ -4,6 +4,7 @@ import com.jgj.byl_process.domain.boards.qna.controller.request.QnaBoardRequest;
 import com.jgj.byl_process.domain.boards.qna.controller.request.QnaCommentRequest;
 import com.jgj.byl_process.domain.boards.qna.entity.QnaBoard;
 import com.jgj.byl_process.domain.boards.qna.service.QnaBoardService;
+import com.jgj.byl_process.domain.boards.qna.service.QnaCommentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,4 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BylProcessApplicationTests {
 
+    @Autowired
+    private QnaCommentService qnaCommentService;
+    
+    @Test
+    public void 게시판_댓글_리스트_테스트() {
+        System.out.println(qnaCommentService.qnaCommentList(4L));
+    }
 }

541302abd3245e129b375b6a5d3c4fb9d8fceae5
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-06 01:50:15 +0900

    test: [박지영] 3l, 4l 모두 주석처리 + Mock 주석처리

--- src/test/java/com/jgj/byl_process/forTest/qnaBoard/QnaBoardTest.java
+++ src/test/java/com/jgj/byl_process/forTest/qnaBoard/QnaBoardTest.java
@@ -13,18 +13,18 @@ public class QnaBoardTest {
     @Autowired
     private QnaBoardService qnaBoardService;
 
-//    @Test
-//    public void 게시글_등록_테스트() {
-//        QnaBoardRequest qnaBoardRequest =
-//                new QnaBoardRequest("test의 test", "박태현", "테스트 내용임");
-//
-//        qnaBoardService.register(qnaBoardRequest);
-//    }
+    @Test
+    public void 게시글_등록_테스트() {
+        QnaBoardRequest qnaBoardRequest =
+                new QnaBoardRequest("test의 test", "박태현", "테스트 내용임");
 
-//    @Test
-//    public void 게시글_리스트_테스트() {
-//        System.out.println(qnaBoardService.list());
-//    }
+        qnaBoardService.register(qnaBoardRequest);
+    }
+
+    @Test
+    public void 게시글_리스트_테스트() {
+        System.out.println(qnaBoardService.list());
+    }
 
 //    @Test
 //    public void 게시글_읽기_테스트() {
@@ -46,24 +46,24 @@ public class QnaBoardTest {
 //        qnaBoardService.read(3L);
 //    }
 //
-//    @Test
-//    public void 게시판_구동_전체_테스트() {
-//        QnaBoardRequest QnaBoardRequest =
-//                new QnaBoardRequest("등록되냐?", "박태현", "등록 되나요?");
-//
-//        qnaBoardService.register(QnaBoardRequest);
-//        Long lastQnaBoardId = qnaBoardService.getLastEntityId();
-//
-//        System.out.println("초기 등록" + qnaBoardService.read(lastQnaBoardId));
-//
-//        qnaBoardService.modify(lastQnaBoardId, new QnaBoardRequest(
-//                "변경 됩니까?", "박태현", "변경 됐어요"
-//        ));
-//
-//        System.out.println("수정 후 : " + qnaBoardService.read(lastQnaBoardId));
-//
-//        qnaBoardService.remove(lastQnaBoardId);
-//
-//        System.out.println("삭제 후 : " + qnaBoardService.read(lastQnaBoardId));
-//    }
+    @Test
+    public void 게시판_구동_전체_테스트() {
+        QnaBoardRequest QnaBoardRequest =
+                new QnaBoardRequest("등록되냐?", "박태현", "등록 되나요?");
+
+        qnaBoardService.register(QnaBoardRequest);
+        Long lastQnaBoardId = qnaBoardService.getLastEntityId();
+
+        System.out.println("초기 등록" + qnaBoardService.read(lastQnaBoardId));
+
+        qnaBoardService.modify(lastQnaBoardId, new QnaBoardRequest(
+                "변경 됩니까?", "박태현", "변경 됐어요"
+        ));
+
+        System.out.println("수정 후 : " + qnaBoardService.read(lastQnaBoardId));
+
+        qnaBoardService.remove(lastQnaBoardId);
+
+        System.out.println("삭제 후 : " + qnaBoardService.read(lastQnaBoardId));
+    }
 }

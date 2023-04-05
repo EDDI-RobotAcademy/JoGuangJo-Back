63f0cc85c29e97365228e3230208031c948ab2c4
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-05 20:54:42 +0900

    test: [박지영] 백엔드 CI 구축 이후 mocking 적용한 테스트 코드 올려보기 [BYL-BACK-PROCESS-27]

---
+++ src/test/java/com/jgj/byl_process/forTest/boards/qna/QnaBoardTest2.java
@@ -0,0 +1,31 @@
+package com.jgj.byl_process.forTest.boards.qna;
+
+import com.jgj.byl_process.domain.boards.qna.controller.request.QnaBoardRequest;
+import com.jgj.byl_process.domain.boards.qna.service.QnaBoardService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("qna 게시판에 대한 테스트 코드 - mock 적용")
+@SpringBootTest
+public class QnaBoardTest2 {
+
+    @Mock
+    private QnaBoardService mockQnaBoardService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void 게시물_저장_테스트() {
+        QnaBoardRequest qnaBoardRequest =
+                new QnaBoardRequest("테스트제목", "테스트작성자", "테스트내용");
+
+        mockQnaBoardService.register(qnaBoardRequest);
+    }
+}

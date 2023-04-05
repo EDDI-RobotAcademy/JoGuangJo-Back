b240d65a97b1f5d82a725ba77b87f886c6282157
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-06 01:25:24 +0900

    test: [박지영] CI 테스트

--- src/test/java/com/jgj/byl_process/forTest/boards/qna/QnaBoardTest2.java
+++ src/test/java/com/jgj/byl_process/forTest/boards/qna/QnaBoardTest2.java
@@ -7,22 +7,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @DisplayName("qna 게시판에 대한 테스트 코드 - mock 적용")
 @SpringBootTest
 public class QnaBoardTest2 {
 
-    @Mock
+//    @Mock
+    @Autowired
     private QnaBoardService mockQnaBoardService;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
+//    @BeforeEach
+//    public void setUp() throws Exception {
+//        MockitoAnnotations.initMocks(this);
+//    }
 
     @Test
-    public void 게시물_저장_테스트() {
+    public void 게시물_등록_테스트() {
         QnaBoardRequest qnaBoardRequest =
                 new QnaBoardRequest("테스트제목", "테스트작성자", "테스트내용");
 

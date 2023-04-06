32f7b4ec613332ce71642c3d3f3c198af559ff24
taylor930422 <125858598+taylor930422@users.noreply.github.com>
2023-04-07 02:34:56 +0900

    refactor: [박태현] 게시글 테스트 코드 리팩터링[BYL-BACK-PROCESS-29]

--- src/test/java/com/jgj/byl_process/BylProcessApplicationTests.java
+++ src/test/java/com/jgj/byl_process/BylProcessApplicationTests.java
@@ -1,11 +1,5 @@
 package com.jgj.byl_process;
 
-import com.jgj.byl_process.domain.boards.qna.controller.request.QnaBoardRequest;
-import com.jgj.byl_process.domain.boards.qna.controller.request.QnaCommentRequest;
-import com.jgj.byl_process.domain.boards.qna.entity.QnaBoard;
-import com.jgj.byl_process.domain.boards.qna.service.QnaBoardService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest

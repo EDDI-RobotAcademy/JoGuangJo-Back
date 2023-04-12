7fc4cbae9149ffb2484dc48b2daef569f5cf87a9
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2023-04-13 03:19:22 +0900

    rename: [안성희] MemberTypeRequest Entity 파일 MemberRoll로 변경[BYL-BACK-PROCESS-39]

--- src/main/java/com/jgj/byl_process/domain/mypage/repository/MemberTypeRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jgj.byl_process.domain.mypage.repository;
-
-import com.jgj.byl_process.domain.mypage.entity.MemberTypeRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberTypeRequestRepository extends JpaRepository<MemberTypeRequest, Long> {
-}
